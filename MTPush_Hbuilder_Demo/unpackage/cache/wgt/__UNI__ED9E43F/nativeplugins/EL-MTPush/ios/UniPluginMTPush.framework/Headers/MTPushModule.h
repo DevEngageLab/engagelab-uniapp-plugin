f8a05c41118151e1aa4c517fe8e4fb6d0f752662
huangshuni <15889638723@163.com>
2024-04-09 18:55:35 +0800

    更新iOS 版本为 4.3.3

---
+++ MTPush_Hbuilder_Demo/unpackage/cache/wgt/__UNI__ED9E43F/nativeplugins/EL-MTPush/ios/UniPluginMTPush.framework/Headers/MTPushModule.h
@@ -0,0 +1,17 @@
+//
+//  MYPushModule.h
+//  UniPluginMTPush
+//
+//  Created by huangshuni on 2021/1/12.
+//
+
+#import <Foundation/Foundation.h>
+#import "DCUniModule.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface MTPushModule : DCUniModule
+
+@end
+
+NS_ASSUME_NONNULL_END
